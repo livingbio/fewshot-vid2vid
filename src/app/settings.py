30e58126d159d2f0dec3cbf9468fe1c6ed8ccfc1
achen353 <andrewchenemail1202@gmail.com>
2021-11-18 10:05:36 +0800

    specify type for pydantic config

--- src/app/settings.py
+++ src/app/settings.py
@@ -2,9 +2,9 @@ from pydantic import BaseSettings, Field
 
 
 class Config(BaseSettings):
-    CLOUDINARY_CLOUD_NAME = Field(..., env="CLOUDINARY_CLOUD_NAME")
-    CLOUDINARY_API_KEY = Field(..., env="CLOUDINARY_API_KEY")
-    CLOUDINARY_API_SECRET = Field(..., env="CLOUDINARY_API_SECRET")
+    CLOUDINARY_CLOUD_NAME: str = Field(..., env="CLOUDINARY_CLOUD_NAME")
+    CLOUDINARY_API_KEY: str = Field(..., env="CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET: str = Field(..., env="CLOUDINARY_API_SECRET")
 
 
 config = Config()
